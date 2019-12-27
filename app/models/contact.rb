70f4ba156f98c778595573c0611ceb43ed393b5b
Pop <watilafreire26@gmail.com>
2019-12-27 18:20:32 -0300

    crud dos nested_attributes em contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
   belongs_to :kind
   has_many :phones
-  accepts_nested_attributes_for :phones
+  accepts_nested_attributes_for :phones, allow_destroy: true
 
   # def country
   #   'Brasil'
