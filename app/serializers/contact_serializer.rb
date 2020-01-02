8572587aaf8b08dc43372b482322509cc67f3a08
Pop <watilafreire26@gmail.com>
2020-01-02 15:00:17 -0300

    Formatando data birthdate de acordo com a ISO 8601

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -3,7 +3,8 @@ class ContactSerializer < ActiveModel::Serializer
 
   def attributes(*args)
     hash = super(*args)
-    hash[:birthdate] = (I18n.l(object.birthdate) if object.birthdate.present?)
+    # hash[:birthdate] = (I18n.l(object.birthdate) if object.birthdate.present?) # -> Formato pt-br
+    hash[:birthdate] = object.birthdate.to_time.iso8601 if object.birthdate.present? # -> Formato da norma ISO 8601
     hash
   end
 end
